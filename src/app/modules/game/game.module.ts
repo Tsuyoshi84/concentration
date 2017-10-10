5580abdda43a2ebef74166b59050da5ab3251300
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2017-10-10 20:32:32 +0900

    Add Card component that represents a card.

--- src/app/modules/game/game.module.ts
+++ src/app/modules/game/game.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { GameRoutingModule } from './game-routing.module';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GameComponent } from './components/game/game.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GameRoutingModule
   ],
-  declarations: [CardListComponent, GameComponent],
+  declarations: [CardListComponent, GameComponent, CardComponent],
   exports: [GameComponent]
 })
 export class GameModule { }
