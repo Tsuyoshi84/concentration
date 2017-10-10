a971ea74d4441f9ca5fedcd1995d57b9edf82120
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2017-10-10 20:26:33 +0900

    Add card list component.

---
+++ src/app/modules/game/components/card-list/card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'co-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.sass']
+})
+export class CardListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/game/game.module.ts
+++ src/app/modules/game/game.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GameRoutingModule } from './game-routing.module';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GameRoutingModule
   ],
-  declarations: []
+  declarations: [CardListComponent],
 })
 export class GameModule { }
