b23be6279c8ebabda72cbcf70a33d3aeed912b4c
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2017-10-10 20:29:13 +0900

    Modify AppComponent to show GameComponent in the main screen.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { GameModule } from './modules/game/game.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GameModule
   ],
   providers: [],
   bootstrap: [AppComponent]
