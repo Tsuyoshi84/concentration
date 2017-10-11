963ab81503495ef9f871ab8a40bd73ab9fa835f2
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2017-10-12 03:10:18 +0900

    Add navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NavigationModule } from './modules/navigation/navigation.module';
 import { GameModule } from './modules/game/game.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NavigationModule,
     GameModule
   ],
   providers: [],
