0ef3f153058630ebd91d77dfd94f03c0ca521f13
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2018-05-27 21:56:49 +0900

    ナビゲーションバーを削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { NavigationModule } from './modules/navigation/navigation.module';
 import { GameModule } from './modules/game/game.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,17 +9,16 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavigationModule,
     GameModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
