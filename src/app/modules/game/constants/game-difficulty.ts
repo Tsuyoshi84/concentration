8e105af1f16057fc9e6f196b5e8e8bdcb9746d54
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2019-11-04 22:54:28 +0900

    Add hover effect

--- src/app/modules/game/constants/game-difficulty.ts
+++ src/app/modules/game/constants/game-difficulty.ts
@@ -1,8 +1,8 @@
 const gameDifficulty = [
-  { label: 'やさしい😀', num: 8, level: 1 },
-  { label: 'ふつう🙂', num: 16, level: 2 },
-  { label: 'むずかしい🙁', num: 30, level: 3 },
-  { label: 'げろむず🤮', num: 54, level: 4 }
+  { label: 'やさしい', num: 8, level: 1, icon: '😀' },
+  { label: 'ふつう', num: 16, level: 2, icon: '🙂' },
+  { label: 'むずかしい', num: 30, level: 3, icon: '🙁' },
+  { label: 'げろむず', num: 54, level: 4, icon: '🤮' }
 ];
 
 export default gameDifficulty;
