c3811cad062d9f980bd21cac6793cab0f4d842e0
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-09-19 10:17:24 +0900

    Change top page

--- src/app/modules/game/constants/game-difficulty.ts
+++ src/app/modules/game/constants/game-difficulty.ts
@@ -1,8 +1,8 @@
 const gameDifficulty = [
-  { label: 'やさしい', num: 8, level: 1, icon: '😀' },
-  { label: 'ふつう', num: 16, level: 2, icon: '🙂' },
-  { label: 'むずかしい', num: 30, level: 3, icon: '🙁' },
-  { label: 'げろむず', num: 54, level: 4, icon: '🤮' },
+  { label: 'EASY', num: 8, level: 1, icon: '😀' },
+  { label: 'NORMAL', num: 16, level: 2, icon: '🙂' },
+  { label: 'DIFFICULT', num: 30, level: 3, icon: '🙁' },
+  { label: 'INSANE', num: 54, level: 4, icon: '🤮' },
 ];
 
 export default gameDifficulty;
