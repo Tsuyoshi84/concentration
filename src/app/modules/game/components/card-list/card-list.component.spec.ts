e4202a1a2b9431f812f49bc295fe4433edd3b02e
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-12-05 20:58:20 +0900

    Update Angular to 11.x

--- src/app/modules/game/components/card-list/card-list.component.spec.ts
+++ src/app/modules/game/components/card-list/card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CardListComponent } from './card-list.component';
 import { CardComponent } from '../card/card.component';
@@ -8,7 +8,7 @@ describe('CardListComponent', () => {
   let component: CardListComponent;
   let fixture: ComponentFixture<CardListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CardListComponent, CardComponent],
     }).compileComponents();
