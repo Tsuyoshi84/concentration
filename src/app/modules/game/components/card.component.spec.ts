f5dbed1afade65af59fa0e9a4363b2a682ac3eba
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-12-31 18:06:47 +0900

    Refactor CardComponent tests

--- src/app/modules/game/components/card.component.spec.ts
+++ src/app/modules/game/components/card.component.spec.ts
@@ -1,43 +1,24 @@
-import { DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import type { Card } from '../types';
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
-  let cardEl: DebugElement;
-  let card: Card;
+  const card = { id: 1, character: 'A', flipped: false, done: false };
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [CardComponent],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
-    component = fixture.componentInstance;
-
-    card = { id: 1, character: 'A', flipped: false, done: false };
-    component.card = card;
-    fixture.detectChanges();
-
-    cardEl = fixture.debugElement.query(By.css('.button'));
+  it('disabled should be true when flipped', () => {
+    const comp = new CardComponent();
+    comp.card = { ...card, flipped: true };
+    expect(comp.disabled).toBe(true);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('disabled should be true when done', () => {
+    const comp = new CardComponent();
+    comp.card = { ...card, done: true };
+    expect(comp.disabled).toBe(true);
   });
 
   it('should raise flipped event when clicked', () => {
-    let clickedCard!: Card;
-    component.clicked.subscribe((c: Card) => {
-      clickedCard = c;
-    });
-
-    cardEl.triggerEventHandler('click', null);
-    expect(clickedCard).toBe(card);
+    const comp = new CardComponent();
+    comp.card = card;
+    comp.clicked.subscribe((c) => expect(c).toBe(card));
+    comp.onClicked();
   });
 });
