19d1beec3c25aca2c1af8926895025c355c0847c
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-12-31 11:25:57 +0900

    Add top-title component and styles

---
+++ src/app/modules/game/components/top-title/top-title.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-title',
+  templateUrl: './top-title.component.html',
+  styleUrls: ['./top-title.component.css'],
+})
+export class TopTitleComponent {}
