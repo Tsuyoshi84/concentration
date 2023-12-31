f53db34fcc5b2ac7587d89fd89bd2e31a14106e1
Tsuyoshi84 <Tsuyoshi84@users.noreply.github.com>
2023-12-31 00:51:19 +0000

    Apply automatic changes

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	extends: [
-		'stylelint-config-standard',
-		'stylelint-prettier/recommended',
-		'stylelint-config-pretty-order',
-	],
-	plugins: ['stylelint-use-logical-spec'],
-	rules: {
-		'liberty/use-logical-spec': 'always',
-	},
-}
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-prettier/recommended',
+    'stylelint-config-pretty-order',
+  ],
+  plugins: ['stylelint-use-logical-spec'],
+  rules: {
+    'liberty/use-logical-spec': 'always',
+  },
+};
